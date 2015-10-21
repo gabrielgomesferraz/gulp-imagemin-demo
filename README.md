3c552c31b791f8a60ecb272b5de531b614003454
Gabriel Gomes <gabrielgomes639@gmail.com>
2015-10-21 11:56:12 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gulp-imagemin-demo
+
+Run commands in paste the project.
+```
+npm install
+```
+```
+gulp images
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * Front-End BoilerPlate various frameworks project
+ *
+ */
+
+'use strict';
+
+// This gulpfile makes use of new JavaScript features.
+// Babel handles this without us having to do anything. It just works.
+// You can read more about the new JavaScript features here:
+// https://babeljs.io/docs/learn-es2015/
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins();
+
+
+// Optimize images
+gulp.task('images', () =>
+  gulp.src('app/assets/images/**')
+    .pipe($.imagemin({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('build/images'))
+);
